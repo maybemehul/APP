87096c32a81f7e210eca47717f41c825cf0cb0d3
maybemehul <159686502+maybemehul@users.noreply.github.com>
2024-10-29 08:16:58 +0530

    Update LocationController.java

--- LocationController.java
+++ LocationController.java
@@ -1,1 +1,13 @@
+@RestController
+@RequestMapping("/api/locations")
+public class LocationController {
+
+    @Autowired
+    private LocationRepository locationRepository;
+
+    @GetMapping
+    public List<Location> getAllLocations() {
+        return locationRepository.findAll();
+    }
+}
 
